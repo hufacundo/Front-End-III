91b962c7de4d89d45b368e269446e8c0b3e185b9
Hugo Facundo <89493574+hufacundo@users.noreply.github.com>
2023-03-13 22:18:35 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Adicao from "./components/Adicao";
+import Divisao from "./components/Divisao";
+import Multiplicacao from "./components/Multiplicacao";
+import PrecisoEstudar from "./components/PrecisoEstudar";
+import Subtracao from "./components/Subtracao";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Adicao num1={1} num2={2} />
+      <Subtracao num5={5} num3={3}/>
+      <Multiplicacao num2={2} num5={5}/>
+      <Divisao num20={20} num2={2}/>
+      <PrecisoEstudar nomeDaTecnologia="React"/>
     </div>
   );
 }
