398c51cfcd3bc8537c04e311b7df2d0f4f4c2e98
Hugo Facundo <89493574+hufacundo@users.noreply.github.com>
2023-03-13 22:25:06 -0300

    Add files via upload

---
+++ aula1303/src/components/Subtracao.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Subtracao = (props) => {
+  return (
+    <div>
+        <h1>A subtração de {props.num5} - {props.num3} é igual a {props.num5-props.num3}</h1>
+    </div>
+  )
+}
+
+export default Subtracao
