621fbd035a950fba677d369c19b27c4e5c580ca1
Hugo Facundo <89493574+hufacundo@users.noreply.github.com>
2023-04-18 21:06:26 -0300

    Add files via upload

---
+++ aula_1704/src/App.jsx
@@ -0,0 +1,50 @@
+import FormularioDeContato from "./components/FormularioDeContato";
+
+function App() {
+  return (
+    <div>
+      <h1>Formulário de Contato</h1>
+      <FormularioDeContato />
+    </div>
+  );
+}
+
+export default App;
+
+
+/*import Campanha from "./components/Campanha"
+
+function App() {
+  
+  return (
+    <div>
+      <Campanha mes="novembro" />
+    </div>
+  )
+}
+
+export default App*/
+
+// return (
+  //   <div>
+  //     <Campanha mes="novembro"/>
+  //   </div>
+      
+    
+  // )
+
+  // return (
+  //   <div>
+  //     <Exercicio2/>
+  //   </div>
+      
+    
+  // )
+  
+  // return (
+  //   <div>
+  //     <Exercicio1/>
+  //   </div>
+      
+    
+  // )
---
+++ aula_1704/src/components/Exercicio1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercicio1 = () => {
+  return (
+    <div>
+        <p>Teste de CSS</p>
+        <img src='https://pm1.narvii.com/6473/0ba6d77bfa9efd0b9ce309b28e75141fb199b02d_00.jpg'/>
+    </div>
+  )
+}
+
+export default Exercicio1
